d9aacc871b32e19dc8c91bb1e587cf17ede9b94b
Nathan <nathan@novek.io>
2021-11-25 09:50:36 +0300

    feature(test): create basic tests for snippetbox

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"flag"
+	"github.com/aitumik/snippetbox/pkg"
 	"log"
 	"net/http"
 )
 
 func main() {
+
+	cfg := new(pkg.Config)
+	flag.StringVar(&cfg.Addr,"addr",":4000","HTTP Network Address")
+	flag.StringVar(&cfg.StaticDir,"static-dir","./ui/static","Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/",home)
 	mux.HandleFunc("/snippet",showSnippet)
 	mux.HandleFunc("/snippet/create",createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 
 	mux.Handle("/static/",http.StripPrefix("/static",fileServer))
 
-	log.Println("Server started at :4000")
+	log.Printf("Server started at %s",cfg.Addr)
 
-	err := http.ListenAndServe(":4000",mux)
+	err := http.ListenAndServe(cfg.Addr,mux)
 	log.Fatal(err)
 }
