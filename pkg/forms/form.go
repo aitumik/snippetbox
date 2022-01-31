1778ae0854214d89df37fe648bceebd4dac70257
aitumik <aitumik@protonmail.com>
2022-01-31 08:12:46 +0300

    feature(validation): add validation for min length and match a given pattern

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -3,9 +3,12 @@ package forms
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
+
+var EmailRX = regexp.MustCompile("[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z0-9]+")
 
 type Form struct {
 	url.Values
@@ -55,6 +58,28 @@ func (f *Form) PermittedValues(field string, opts ...string) {
 		}
 	}
 	f.Errors.Add(field,"This field is invalid")
+}
+
+func (f *Form) MinLength(field string, l int) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+
+	if utf8.RuneCountInString(value) < l {
+		f.Errors.Add(field,fmt.Sprintf("This field is too short(minimum is %d)",l))
+	}
+}
+
+func (f *Form) MatchesPattern(field string,pattern *regexp.Regexp) {
+	value := f.Get(field)
+	if value == "" {
+		return
+	}
+
+	if !pattern.MatchString(value) {
+		f.Errors.Add(field,"This field is invalid")
+	}
 }
 
 func (f *Form) Valid() bool {
