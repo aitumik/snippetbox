1260faa552fa153e8537d0de62e196566799babc
aitumik <aitumik@protonmail.com>
2022-01-31 08:13:28 +0300

    feature(signup): implement signup functionality for creating users

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -93,7 +93,33 @@ func (app *application) signupUserForm(w http.ResponseWriter,r *http.Request)  {
 }
 
 func (app *application) signupUser(w http.ResponseWriter,r *http.Request) {
-	fmt.Fprintln(w,"Create new user")
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w,http.StatusBadRequest)
+		return
+	}
+
+	form := forms.New(r.PostForm)
+	form.Required("name","email","password")
+	form.MatchesPattern("email",forms.EmailRX)
+	form.MinLength("password",10)
+
+	if !form.Valid() {
+		data := &TemplateData{
+			Form: form,
+		}
+		app.render(w,r,"signup.page.tmpl",data)
+		return
+	}
+
+	err = app.users.Insert(form.Get("name"),form.Get("email"),form.Get("password"))
+	if err != nil {
+		app.serverError(w,err)
+		return
+	}
+
+	app.session.Put(r,"flash","Registration successful. Please login.")
+	http.Redirect(w,r,"/user/login",http.StatusSeeOther)
 }
 
 func (app *application) loginUserForm(w http.ResponseWriter,r *http.Request) {
