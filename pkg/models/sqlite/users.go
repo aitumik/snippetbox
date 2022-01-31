fb83404ea019614c8d0147f8a993098e713eea26
aitumik <aitumik@protonmail.com>
2022-01-31 08:14:21 +0300

    feature(models): implement user model method  which inserts new users into the database

--- pkg/models/sqlite/users.go
+++ pkg/models/sqlite/users.go
@@ -3,14 +3,26 @@ package sqlite
 import (
 	"database/sql"
 	"github.com/aitumik/snippetbox/pkg/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserModel struct {
 	DB *sql.DB
 }
 
-func (m *UserModel) Insert(email,password string) error {
-	return nil
+func (m *UserModel) Insert(name,email,password string) error {
+	HasedPassword, err := bcrypt.GenerateFromPassword([]byte(password),12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users(name,email,hashed_password,created) VALUES(?,?,?,TIME())`
+	// now you have the has
+	_, err = m.DB.Exec(stmt, name, email, string(HasedPassword))
+	if err != nil {
+		return err
+	}
+	return err
 }
 
 func (m *UserModel) Authenticate(email,password string) (int,error) {
