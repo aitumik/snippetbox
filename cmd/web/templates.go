76e3dab800e88c108424a3bdcfd7bd5b8baa2456
aitumik <aitumik@protonmail.com>
2022-02-02 05:47:47 +0300

    I am changing. I am growing.I am becoming better

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -9,19 +9,20 @@ import (
 )
 
 type TemplateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
-	CurrentYear int
-	Flash string
-	Form *forms.Form
+	Snippet           *models.Snippet
+	Snippets          []*models.Snippet
+	CurrentYear       int
+	Flash             string
+	Form              *forms.Form
+	AuthenticatedUser int
 }
 
 func NewTemplateCache(dir string) (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
-	pages,err := filepath.Glob(filepath.Join(dir,"*.page.tmpl"))
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 	for _, page := range pages {
 		// Extract the file name(like 'home.page.tmpl') from the full path
@@ -30,19 +31,19 @@ func NewTemplateCache(dir string) (map[string]*template.Template, error) {
 
 		// The template.FuncMap must be registered with the template set before the ParseFiles
 		// method
-		ts,err := template.New(name).Funcs(functions).ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
 		if err != nil {
-			return nil,err
+			return nil, err
 		}
 
 		cache[name] = ts
