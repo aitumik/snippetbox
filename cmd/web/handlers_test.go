48d64f0d94c01684b5306956aecf771ad8090e1e
aitumik <aitumik@protonmail.com>
2022-02-06 15:54:35 +0300

    feat(test): write tests for showSnippet handler

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"testing"
 )
@@ -19,5 +20,40 @@ func TestPing(t *testing.T) {
 	if string(body) != "OK" {
 		t.Errorf("expteced body to contain %v","OK")
 	}
+}
 
+func TestShowSnippet(t *testing.T) {
+	// Create an instance of the application using `netTestApplication() function`
+	app := newTestApplication(t)
+
+	// Create a server
+	ts := newTestServer(t,app.routes())
+	defer ts.Close()
+
+	tests := []struct{
+		name string
+		urlPath string
+		wantCode int
+		wantBody []byte
+	} {
+		{"Valid ID","/snippets/1",http.StatusOK,[]byte("I never meant for any of this to happen")},
+		{"Non existent ID","/snippets/2",http.StatusNotFound,nil},
+		{"Negative ID","/snippets/-10",http.StatusNotFound,nil},
+		{"Empty ID","/snippets/",http.StatusNotFound,nil},
+		{"Trailing Slash","/snippets/1/",http.StatusNotFound,nil},
+		{"String ID","/snippets/foo",http.StatusNotFound,nil},
+		{"Decimal ID","/snippets/1.3",http.StatusNotFound,nil},
+	}
+	for _,tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code,_,body := ts.get(t,tt.urlPath)
+			if code != tt.wantCode {
+				t.Errorf("expected %d; got %d",tt.wantCode,code)
+			}
+
+			if !bytes.Contains(body,tt.wantBody) {
+				t.Errorf("expected body to contain %q", tt.wantBody)
+			}
+		})
+	}
 }
