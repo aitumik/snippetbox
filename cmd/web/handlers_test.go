4cce42c1b3e8754eca905a4ec57542581243fbe6
aitumik <aitumik@protonmail.com>
2022-02-06 16:03:24 +0300

    fix(chore): run gofmt on handlers tests

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -8,17 +8,17 @@ import (
 
 func TestPing(t *testing.T) {
 	app := newTestApplication(t)
-	ts := newTestServer(t,app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	code,_,body := ts.get(t,"/ping")
+	code, _, body := ts.get(t, "/ping")
 
 	if code != http.StatusOK {
-		t.Errorf("expected %q; got%q",http.StatusOK,code)
+		t.Errorf("expected %q; got%q", http.StatusOK, code)
 	}
 
 	if string(body) != "OK" {
-		t.Errorf("expteced body to contain %v","OK")
+		t.Errorf("expteced body to contain %v", "OK")
 	}
 }
 
@@ -27,31 +27,31 @@ func TestShowSnippet(t *testing.T) {
 	app := newTestApplication(t)
 
 	// Create a server
-	ts := newTestServer(t,app.routes())
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	tests := []struct{
-		name string
-		urlPath string
+	tests := []struct {
+		name     string
+		urlPath  string
 		wantCode int
 		wantBody []byte
-	} {
-		{"Valid ID","/snippets/1",http.StatusOK,[]byte("I never meant for any of this to happen")},
-		{"Non existent ID","/snippets/2",http.StatusNotFound,nil},
-		{"Negative ID","/snippets/-10",http.StatusNotFound,nil},
-		{"Empty ID","/snippets/",http.StatusNotFound,nil},
-		{"Trailing Slash","/snippets/1/",http.StatusNotFound,nil},
-		{"String ID","/snippets/foo",http.StatusNotFound,nil},
-		{"Decimal ID","/snippets/1.3",http.StatusNotFound,nil},
+	}{
+		{"Valid ID", "/snippets/1", http.StatusOK, []byte("I never meant for any of this to happen")},
+		{"Non existent ID", "/snippets/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippets/-10", http.StatusNotFound, nil},
+		{"Empty ID", "/snippets/", http.StatusNotFound, nil},
+		{"Trailing Slash", "/snippets/1/", http.StatusNotFound, nil},
+		{"String ID", "/snippets/foo", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippets/1.3", http.StatusNotFound, nil},
 	}
-	for _,tt := range tests {
+	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			code,_,body := ts.get(t,tt.urlPath)
+			code, _, body := ts.get(t, tt.urlPath)
 			if code != tt.wantCode {
-				t.Errorf("expected %d; got %d",tt.wantCode,code)
+				t.Errorf("expected %d; got %d", tt.wantCode, code)
 			}
 
-			if !bytes.Contains(body,tt.wantBody) {
+			if !bytes.Contains(body, tt.wantBody) {
 				t.Errorf("expected body to contain %q", tt.wantBody)
 			}
 		})
