82c386809742c49e5299015399d63c6a899c3fee
aitumik <aitumik@protonmail.com>
2022-02-21 08:13:56 +0300

    fix(ui): fix ui mistakes

--- pkg/forms/form.go
+++ pkg/forms/form.go
@@ -8,7 +8,7 @@ import (
 	"unicode/utf8"
 )
 
-var EmailRX = regexp.MustCompile("[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z0-9]+")
+var EmailRX = regexp.MustCompile(`[a-zA-Z]+@[a-zA-Z]+\\.[a-zA-Z0-9]+`)
 
 type Form struct {
 	url.Values
@@ -24,25 +24,25 @@ func New(data url.Values) *Form {
 
 // Required check if the field is empty or not
 func (f *Form) Required(fields ...string) {
-	for _,field := range fields {
+	for _, field := range fields {
 		value := f.Get(field)
 
 		if strings.TrimSpace(value) == "" {
-			f.Errors.Add(field,"This field cannot be blank")
+			f.Errors.Add(field, "This field cannot be blank")
 		}
 	}
 }
 
 // MaxLength Implement a MaxLength method to check that a specific field in the form
 // doesn't exceed the required characters
-func (f *Form) MaxLength(field string,l int) {
+func (f *Form) MaxLength(field string, l int) {
 	value := f.Get(field)
 	if value == "" {
 		return
 	}
 
 	if utf8.RuneCountInString(value) > l {
-		f.Errors.Add(field,fmt.Sprintf("This field is too long(maximum is %d)",l))
+		f.Errors.Add(field, fmt.Sprintf("This field is too long(maximum is %d)", l))
 	}
 }
 
@@ -52,12 +52,12 @@ func (f *Form) PermittedValues(field string, opts ...string) {
 		return
 	}
 
-	for _,opt := range opts {
+	for _, opt := range opts {
 		if value == opt {
 			return
 		}
 	}
-	f.Errors.Add(field,"This field is invalid")
+	f.Errors.Add(field, "This field is invalid")
 }
 
 func (f *Form) MinLength(field string, l int) {
@@ -67,18 +67,18 @@ func (f *Form) MinLength(field string, l int) {
 	}
 
 	if utf8.RuneCountInString(value) < l {
-		f.Errors.Add(field,fmt.Sprintf("This field is too short(minimum is %d)",l))
+		f.Errors.Add(field, fmt.Sprintf("This field is too short(minimum is %d)", l))
 	}
 }
 
-func (f *Form) MatchesPattern(field string,pattern *regexp.Regexp) {
+func (f *Form) MatchesPattern(field string, pattern *regexp.Regexp) {
 	value := f.Get(field)
 	if value == "" {
 		return
 	}
 
 	if !pattern.MatchString(value) {
-		f.Errors.Add(field,"This field is invalid")
+		f.Errors.Add(field, "This field is invalid")
 	}
 }
 
